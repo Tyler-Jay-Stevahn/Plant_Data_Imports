7ea28a75da7b98bdc6e8ffddb294e2b4e936d3b1
Tyler-Jay-Stevahn <tylerjay1996@hotmail.com>
2021-09-29 12:55:05 -0500

    added message exception

--- pull.py
+++ pull.py
@@ -4,6 +4,9 @@ import pandas as pd
 import psycopg2
 from sqlalchemy import create_engine
 
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 ip_address = '127.0.0.1'
 api_key = 'efnpmWkIGsgNhMUsxR23G8tbOcYSZAJR1ZW8H1o1tjGPbi/8QODtiCW0FIdPI/kN5hoJnQWhvCiEs9ELymAt9HrL2TnL8N8VuAinq5Yc3J609yyNXuOC96OQf1njb1XN0hXiTB/x7JfeFZpe8b2ymlE+k8bgJOkqgLMUrZDebbk='
 endpoint = 'settings/inputs'
