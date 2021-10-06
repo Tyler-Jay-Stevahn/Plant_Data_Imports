d17ec4873c1e0501077517add989f55c49c69cc0
Tyler-Jay-Stevahn <tylerjay1996@hotmail.com>
2021-10-06 16:22:43 -0500

    Changed ipaddress for db

--- pull.py
+++ pull.py
@@ -6,6 +6,7 @@ from sqlalchemy import create_engine
 import time
 
 ip_address = '127.0.0.1'
+ip_address_db = '192.168.1.14'
 api_key = 'efnpmWkIGsgNhMUsxR23G8tbOcYSZAJR1ZW8H1o1tjGPbi/8QODtiCW0FIdPI/kN5hoJnQWhvCiEs9ELymAt9HrL2TnL8N8VuAinq5Yc3J609yyNXuOC96OQf1njb1XN0hXiTB/x7JfeFZpe8b2ymlE+k8bgJOkqgLMUrZDebbk='
 endpoint = 'settings/inputs'
 start_epoch = '0'
@@ -16,7 +17,7 @@ end_epoch = str(current_time)
 alchemyengine = create_engine('postgresql+psycopg2://postgres:Stevahn23!!@192.168.1.73:5432/Plant')
 if_exists = 'append'
 
-url = 'https://192.168.1.150/api/measurements/historical/5f1bb58a-7895-442b-a3a1-ed8112128526/F/0/'+start_epoch+'/'+end_epoch
+url = 'https://'+ip_address_db'/api/measurements/historical/5f1bb58a-7895-442b-a3a1-ed8112128526/F/0/'+start_epoch+'/'+end_epoch
 headers = {'Accept': 'application/vnd.mycodo.v1+json',
             'X-API-KEY': api_key}
 response = requests.get(url, headers=headers, verify=False)
@@ -27,7 +28,7 @@ print('Made dataframe for Temperature')
 df.to_sql('Temperature', alchemyengine, if_exists=if_exists)
 print('Written dataframe to Temperature')
 
-url = 'https://192.168.1.150/api/measurements/historical/5f1bb58a-7895-442b-a3a1-ed8112128526/percent/1/'+start_epoch+'/'+end_epoch
+url = 'https://'+ip_address_db'/api/measurements/historical/5f1bb58a-7895-442b-a3a1-ed8112128526/percent/1/'+start_epoch+'/'+end_epoch
 headers = {'Accept': 'application/vnd.mycodo.v1+json',
             'X-API-KEY': api_key}
 response = requests.get(url, headers=headers, verify=False)
@@ -38,7 +39,7 @@ print('Made dataframe for Humidity')
 df.to_sql('Humidity', alchemyengine, if_exists=if_exists)
 print('Written dataframe to Humidity')
 
-url = 'https://192.168.1.150/api/measurements/historical/5f1bb58a-7895-442b-a3a1-ed8112128526/Pa/2/'+start_epoch+'/'+end_epoch
+url = 'https://'+ip_address_db'/api/measurements/historical/5f1bb58a-7895-442b-a3a1-ed8112128526/Pa/2/'+start_epoch+'/'+end_epoch
 headers = {'Accept': 'application/vnd.mycodo.v1+json',
             'X-API-KEY': api_key}
 response = requests.get(url, headers=headers, verify=False)
@@ -50,7 +51,7 @@ df.to_sql('Pressure', alchemyengine, if_exists=if_exists)
 print('Written dataframe to Pressure')
 
 
-url = 'https://192.168.1.150/api/measurements/historical/5f1bb58a-7895-442b-a3a1-ed8112128526/F/3/'+start_epoch+'/'+end_epoch
+url = 'https://'+ip_address_db'/api/measurements/historical/5f1bb58a-7895-442b-a3a1-ed8112128526/F/3/'+start_epoch+'/'+end_epoch
 headers = {'Accept': 'application/vnd.mycodo.v1+json',
             'X-API-KEY': api_key}
 response = requests.get(url, headers=headers, verify=False)
@@ -61,7 +62,7 @@ print('Made dataframe for Dewpoint')
 df.to_sql('Dewpoint', alchemyengine, if_exists=if_exists)
 print('Written dataframe to Dewpoint')
 
-url = 'https://192.168.1.150/api/measurements/historical/5f1bb58a-7895-442b-a3a1-ed8112128526/m/4/'+start_epoch+'/'+end_epoch
+url = 'https://'+ip_address_db'/api/measurements/historical/5f1bb58a-7895-442b-a3a1-ed8112128526/m/4/'+start_epoch+'/'+end_epoch
 headers = {'Accept': 'application/vnd.mycodo.v1+json',
             'X-API-KEY': api_key}
 response = requests.get(url, headers=headers, verify=False)
@@ -72,7 +73,7 @@ print('Made dataframe for Altitude')
 df.to_sql('Altitude', alchemyengine, if_exists=if_exists)
 print('Written dataframe to Altitude')
 
-url = 'https://192.168.1.150/api/measurements/historical/5f1bb58a-7895-442b-a3a1-ed8112128526/Pa/5/'+start_epoch+'/'+end_epoch
+url = 'https://'+ip_address_db'/api/measurements/historical/5f1bb58a-7895-442b-a3a1-ed8112128526/Pa/5/'+start_epoch+'/'+end_epoch
 headers = {'Accept': 'application/vnd.mycodo.v1+json',
             'X-API-KEY': api_key}
 response = requests.get(url, headers=headers, verify=False)
@@ -83,7 +84,7 @@ print('Made dataframe for Vapor Pressure Deficit')
 df.to_sql('Vapor_Pressure_Deficit', alchemyengine, if_exists=if_exists)
 print('Written dataframe to Vapor Pressure Deficit')
 
-url = 'https://192.168.1.150/api/measurements/historical/4a6ba274-3fe1-4815-83cf-d1df94353136/lux/0/'+start_epoch+'/'+end_epoch
+url = 'https://'+ip_address_db'/api/measurements/historical/4a6ba274-3fe1-4815-83cf-d1df94353136/lux/0/'+start_epoch+'/'+end_epoch
 headers = {'Accept': 'application/vnd.mycodo.v1+json',
             'X-API-KEY': api_key}
 response = requests.get(url, headers=headers, verify=False)
